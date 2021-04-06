8d68bdd1a7b049b30448751f6c8732f7df813c38
SBhanushali <shivam.n.bhanushali@gmail.com>
2021-04-06 08:57:59 +0530

    Create color and size constants

---
+++ src/config/theme.js
@@ -0,0 +1,40 @@
+const colors = {
+  white: "#FFFFFF",
+  blue: {
+    10: "#ECF2FF",
+    20: "#D0E2FF",
+    60: "#0F62FE",
+    70: "#054ADA",
+    80: "#002D9C",
+  },
+  gray: {
+    30: "#C6C6C6",
+    50: "#8D8D8D",
+    60: "#4C4C4C",
+    80: "#393939",
+    100: "#161616",
+  },
+  red: {
+    60: "#DA1E28",
+    70: "#BA1B23",
+    80: "#750E13",
+  },
+};
+
+const sizes = {
+  lg: {
+    h: 48,
+    fontSize: 14,
+    paddingX: 16,
+  },
+  md: {
+    h: 40,
+    fontSize: 12,
+    paddingX: 14,
+  },
+  sm: {
+    h: 32,
+    fontSize: 10,
+    paddingX: 12,
+  },
+};
