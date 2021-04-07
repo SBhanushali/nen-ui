d9372f2b4181bad959e752dca9252298b6a728e5
SBhanushali <shivam.n.bhanushali@gmail.com>
2021-04-07 20:04:33 +0530

    Make light and dark theme config

---
+++ src/config/buttonTheme.js
@@ -0,0 +1,63 @@
+import { colors } from "./theme";
+
+export const light = {
+  primary: {
+    color: colors.white,
+    background: colors.blue[60],
+    hoverBackground: colors.blue[70],
+    activeBackground: colors.blue[80],
+    borderColor: colors.blue[60],
+  },
+  secondary: {
+    color: colors.white,
+    background: colors.gray[80],
+    hoverBackground: colors.gray[60],
+    activeBackground: colors.blue[100],
+    borderColor: colors.gray[80],
+  },
+  ghost: {
+    color: colors.blue[60],
+    background: "transparent",
+    hoverBackground: colors.blue[10],
+    activeBackground: colors.blue[20],
+    borderColor: colors.blue[60],
+  },
+  danger: {
+    color: colors.white,
+    background: colors.red[60],
+    hoverBackground: colors.red[70],
+    activeBackground: colors.red[80],
+    borderColor: colors.red[80],
+  },
+};
+
+export const dark = {
+  primary: {
+    color: "#000000",
+    background: colors.blue[60],
+    hoverBackground: colors.blue[70],
+    activeBackground: colors.blue[80],
+    borderColor: colors.blue[60],
+  },
+  secondary: {
+    color: "#000000",
+    background: colors.gray[80],
+    hoverBackground: colors.gray[60],
+    activeBackground: colors.blue[100],
+    borderColor: colors.gray[80],
+  },
+  ghost: {
+    color: colors.blue[60],
+    background: "transparent",
+    hoverBackground: colors.blue[10],
+    activeBackground: colors.blue[20],
+    borderColor: colors.blue[60],
+  },
+  danger: {
+    color: "#000000",
+    background: colors.red[60],
+    hoverBackground: colors.red[70],
+    activeBackground: colors.red[80],
+    borderColor: colors.red[80],
+  },
+};
