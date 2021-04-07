72d2fedf0b197c638592908611a6d684ae4e294e
SBhanushali <shivam.n.bhanushali@gmail.com>
2021-04-07 23:05:30 +0530

    export all the components

--- src/index.js
+++ src/index.js
@@ -1,10 +1,3 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+export * from "./components";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+export * from "./config";
