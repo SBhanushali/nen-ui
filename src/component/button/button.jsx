c646203f9a086c35d71f9b4bcf3f9b8d54a479fe
SBhanushali <shivam.n.bhanushali@gmail.com>
2021-04-07 20:07:08 +0530

    destructure props and use global theme

--- src/component/button/button.jsx
+++ src/component/button/button.jsx
@@ -1,61 +1,52 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { sizes, colors } from "../../config/theme";
-import { variants } from "./buttonVariants";
 import styled from "styled-components";
-
-const mode = "light";
 
 const StyledButton = styled.button`
   height: ${(props) => (props.size ? sizes[props.size].h : "")}px;
   font-size: ${(props) => (props.size ? sizes[props.size].fontSize : "")}px;
   padding: 0 ${(props) => (props.size ? sizes[props.size].paddingX : "")}px;
-  background: ${(props) =>
-    props.variant ? variants[props.variant][mode].background : props.bgColor};
-  color: ${(props) =>
-    props.variant ? variants[props.variant][mode].color : props.color};
+  background: ${({ theme, variant, bgColor }) =>
+    variant ? theme[variant].background : bgColor};
+  color: ${({ theme, variant, color }) =>
+    variant ? theme[variant].color : color};
   border: none;
   &:hover {
-    background: ${(props) =>
-      props.variant
-        ? variants[props.variant][mode].hoverBackground
-        : props.bgColor};
+    background: ${({ theme, variant, bgColor }) =>
+      variant ? theme[variant].hoverBackground : bgColor};
   }
   &:active {
-    background: ${(props) =>
-      props.variant
-        ? variants[props.variant][mode].activeBackground
-        : props.bgColor};
+    background: ${({ theme, variant, bgColor }) =>
+      variant ? theme[variant].activeBackground : bgColor};
   }
   &:focus {
     border: 1px solid ${colors.white};
     outline: 2px solid
-      ${(props) =>
-        props.variant
-          ? variants[props.variant][mode].borderColor
-          : props.bgColor};
+      ${({ theme, variant, bgColor }) =>
+        variant ? theme[variant].borderColor : bgColor};
   }
   &:disabled {
     cursor: not-allowed;
     color: ${colors.gray[50]};
-    background-color: ${(props) =>
-      props.variant === "ghost" ? "transparent" : colors.gray[30]};
+    background-color: ${({ variant }) =>
+      variant === "ghost" ? "transparent" : colors.gray[30]};
   }
 `;
 
 const Icon = styled.span`
-  ${(props) =>
-    props.isOnlyIcon &&
+  ${({ isOnlyIcon, align }) =>
+    isOnlyIcon &&
     `
-  margin-right: ${props.align !== "right" ? 5 : ""}px;
-  margin-left: ${props.align === "right" ? 5 : ""}px;
+  margin-right: ${align !== "right" ? 5 : ""}px;
+  margin-left: ${align === "right" ? 5 : ""}px;
 `}
 `;
 
-const Button = ({ variant, size, children, icon, align, ...rest }) => {
+const Button = ({ variant, size, children, icon, align, title, ...rest }) => {
   const isOnlyIcon = children && true;
   return (
-    <StyledButton variant={variant} size={size} {...rest}>
+    <StyledButton variant={variant} size={size} title={title} {...rest}>
       {icon && align !== "right" && <Icon isOnlyIcon={isOnlyIcon}>{icon}</Icon>}
       {children}
       {icon && align && align === "right" && (
